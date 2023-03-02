c1df5063e0d1af2a75ddcb20ff1f5cedaece5871
Jeanette Stavsholt <jeanette.stavsholt@live.no>
2023-03-02 21:07:01 +0100

    add missing dependency

--- pages/components/UserNew.tsx
+++ pages/components/UserNew.tsx
@@ -1,15 +1,15 @@
-import TextField from "@mui/material/TextField"
-import Typography from "@mui/material/Typography"
-import Box from "@mui/material/Box"
-import { useEffect, useState } from "react"
-import ImageList from "@mui/material/ImageList"
-import ImageListItem from "@mui/material/ImageListItem"
-import { defaultPokemonsArray, ValidDefaultPokemonId } from "../helpers"
-import PokemonImage from "./PokemonImage"
-import { styled } from "@mui/material/styles"
-import useUserSettings from "../useUserSettings"
-import { useRouter } from "next/router"
-import { routes } from "../routes"
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { defaultPokemonsArray, ValidDefaultPokemonId } from "../helpers";
+import PokemonImage from "./PokemonImage";
+import { styled } from "@mui/material/styles";
+import useUserSettings from "../useUserSettings";
+import { useRouter } from "next/router";
+import { routes } from "../routes";
 
 const GabrielsTextField = styled(TextField)({
   "& label.Mui-focused": {
@@ -29,48 +29,49 @@ const GabrielsTextField = styled(TextField)({
       borderColor: "yellow",
     },
   },
-})
+});
 
 const UserNew = () => {
-  const [name, setName] = useState("")
-  const [defaultPokemon, setDefaultPokemon] = useState<ValidDefaultPokemonId>(1)
-  const { setUserSettings } = useUserSettings()
-  const router = useRouter()
+  const [name, setName] = useState("");
+  const [defaultPokemon, setDefaultPokemon] =
+    useState<ValidDefaultPokemonId>(1);
+  const { setUserSettings } = useUserSettings();
+  const router = useRouter();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value)
-  }
+    setName(event.target.value);
+  };
   const handleClickPlay = () => {
     setUserSettings([
       ["name", name],
       ["pokemonOfChoice", defaultPokemon],
-    ])
-    router.push(routes.game)
-  }
+    ]);
+    router.push(routes.game);
+  };
 
   useEffect(() => {
     // Prefetch the dashboard page
-    router.prefetch(routes.game)
-  }, [])
+    router.prefetch(routes.game);
+  }, [router]);
 
   return (
     <Box textAlign={"center"}>
       <Box mb={5}>
-        <Typography variant='h4' gutterBottom>
+        <Typography variant="h4" gutterBottom>
           Hva heter du?
         </Typography>
         <GabrielsTextField
-          id='outlined-name'
-          label='Name'
+          id="outlined-name"
+          label="Name"
           InputProps={{ style: { fontSize: "xx-large" } }}
-          color='secondary'
+          color="secondary"
           fullWidth
           value={name}
           onChange={handleChange}
         />
       </Box>
       <Box mb={5}>
-        <Typography variant='h4'>Velg pokémon</Typography>
+        <Typography variant="h4">Velg pokémon</Typography>
 
         <ImageList sx={{ height: 105 }} cols={3} rowHeight={100} gap={1}>
           {defaultPokemonsArray.map((id: ValidDefaultPokemonId) => (
@@ -89,9 +90,9 @@ const UserNew = () => {
           ))}
         </ImageList>
       </Box>
-      <Box className='rainbow-button playmore' onClick={handleClickPlay}></Box>
+      <Box className="rainbow-button playmore" onClick={handleClickPlay}></Box>
     </Box>
-  )
-}
+  );
+};
 
-export default UserNew
+export default UserNew;
